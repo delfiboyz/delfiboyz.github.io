81676346bc5728574609fc0bab6031d1277156a7
delfiboyz <delfiboyz@gmail.com>
2024-03-27 00:52:51 +0700

    commit
    commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    
+    "about.html",
+    "classpage.html",
+    "home.html",
+    "kontak.html",
+    "programpage.html",
+    "trialpage.html"
+  ],
+    theme: {
+      extend: {
+        colors: {
+          'custom-gray': '#333333',
+          'custom-orange': '#FFA500',
+          'custom-light-gray': '#F7F7F7',
+          'custom-blue': '#0044CC',
+          'custom-teal': '#00CCCC',
+        },
+        maxWidth: {
+          prose: '65ch',
+        },
+        boxShadow: {
+          'xl': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
+          '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+        }
+      }
+    }
+  }
+  
