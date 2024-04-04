c3b195b1589d87b3b8e0862b4663cd4991198572
delfi boyz <delfiboyz@gmail.com>
2024-04-05 02:13:19 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
         boxShadow: {
           'xl': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
           '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+          
         }
       }
     }
